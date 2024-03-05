0064d68f4d4094333843e3bb513abc54bc40c71b
Hmarkov <icko541@hotmail.it>
2024-03-05 04:16:16 +0000

    Add broken selenium ToFix

--- LinkedinScraper/Scraper/Scraper.cs
+++ LinkedinScraper/Scraper/Scraper.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LinkedinScraper.Models;
 
 namespace LinkedinScraper.Scraper
 {
-    internal class Scraper
-    {
+    public class Scraper {
+        private readonly string _linkedinUrl;
+
+        public Scraper(string url)
+        {
+            _linkedinUrl = url;
+        }
+
+        public async Task<List<Job>> Scrape() {
+            var jobs = new List<Job>();
+            string pageContent = null;
+        }
+
+        async Task<string> FetchPageContentAsync()
+        {
+            var options = new ChromeOptions()
+            {
+                BinaryLocation = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+            };
+
+            options.AddArguments(new List<string>() { "headless", "disable-gpu" });
+
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                // Navigate to the desired URL
+                driver.Navigate().GoToUrl("https://example.com");
+
+                // Fetch page content
+                return await Task.Run(() => driver.PageSource);
+            }
+        }
+
+
     }
 }
