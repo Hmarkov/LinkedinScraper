be313bdeaf56b9ae86a1f9c5da657818733f39a5
Hmarkov <icko541@hotmail.it>
2024-03-05 02:42:50 +0000

    Scraper init

---
+++ LinkedinScraper/Models/Job.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedinScraper.Models
+{
+    public class Job {
+        public string Title {  get; set; }
+        public string Description { get; set; }
+        public string CompanyName {  get; set; }
+    }
+ 
+}
